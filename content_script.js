19f34c7964f56b0be92e9065cd77b891091e845d
nav01 <nav01@mail.fresnostate.edu>
2017-05-31 19:08:18 -0700

    Made xmlhttp requests asynchronous.  Accounted for zero search results and zero ratings results.

--- content_script.js
+++ content_script.js
@@ -1,9 +1,8 @@
 function getRating(instructorName){
     instructorQuery = instructorName.replace(" ","+");
-    var xhr = new XMLHttpRequest();
     //There is no official api for ratemyprofessor so it has to be done through unnofficial means.
     //This has the overhead of receiving an entire html webpage as a response, but it's the best
-    //that can be done.
+    //that can be done.  Caching results can help alleviate this.
     var query = `http://www.ratemyprofessors.com/search.jsp?query=${instructorQuery}+california+state+university+fresno`;
     
     chrome.runtime.sendMessage(({query: query}), function(response) {
@@ -14,13 +13,13 @@ function getRating(instructorName){
 //Class information lies in an iframe, but fortunately it is from the same domain so 
 //it can be manipulated.
 var iframeId = 'ptifrmtgtframe';
-//The instructor names lie in a span tag with an id pattern of MTG_INSTR$#
-//where # is the order the class appears in the search results.
-var instructorElementIds = "span[id*='MTG_INSTR$']";
 var iFrame = document.getElementById(iframeId);
 
 $(iFrame).on("load", function () {
     iframeContents = $(iFrame).contents();
+    //The instructor names lie in a span tag with an id pattern of MTG_INSTR$#
+    //where # is the order the class appears in the search results.
+    var instructorElementIds = "span[id*='MTG_INSTR$']";
     $(iframeContents).on('mouseover',instructorElementIds, function() {
         //console.log($(this).text());
         getRating($(this).text());
--- event_page.js
+++ event_page.js
@@ -1,44 +1,71 @@
-//Extracts the links to the instructor rating details from the ratemyprofessor search results.
+
+//Returns an array of links, or undefined if there are no results.
 function extractLinks(doc){
-    const resultListContainerId = "#searchResultsBox";
-    const instructorItemClass = ".listing.PROFESSOR";
-    var resultsList = $(doc).find(resultListContainerId).find(instructorItemClass);
+    const RESULT_LIST_CONTAINER_ID = "#searchResultsBox";
+    const INSTRUCTOR_ITEM_CLASS = ".listing.PROFESSOR";
+    const RESULT_COUNT_CLASS = ".result-count";
+    const NO_RESULTS = "Your search didn't return any results."
+    
+    var resultsList = $(doc).find(RESULT_LIST_CONTAINER_ID).find(INSTRUCTOR_ITEM_CLASS);
+    if($(resultsList).find(RESULT_COUNT_CLASS).text().trim() == NO_RESULTS){
+        console.log("y tho");
+        return undefined;
+    }
     var ratingLinks = [];
     $(resultsList).find('a').each(function() {
         ratingLinks.push("http://www.ratemyprofessors.com" + $(this).attr('href'));
     });
     return ratingLinks;
 }
-//Gets the ratings from the links obtained by the extractLinks function.
-function getRatings(links){
-    const ratingContainerClass = ".left-breakdown";
-    const overallRatingContainerClass = ".breakdown-container.quality";
-    const difficultyRatingContainerClass = ".breakdown-section.difficulty";
-    const ratingClass = ".grade";
-    var ratings = [];
-    for(var i = 0; i < links.length; i++){
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET",links[0],false);
-        xhr.send();
-        ratingPage = $.parseHTML(xhr.responseText,undefined);
-        let overallRating = $(ratingPage).find(ratingContainerClass).find(overallRatingContainerClass).find(ratingClass).text().trim();
-        let difficultyRating = $(ratingPage).find(ratingContainerClass).find(difficultyRatingContainerClass).find(ratingClass).text().trim();
-        ratings.push({overall: overallRating, difficulty: difficultyRating});
+//Gets the ratings from the links obtained by the extractLinks function. (Recursive)
+//Sends an array of objects back to the content script.
+function getRatings(links, sendResponse, ratings = []){
+    const RATING_CONTAINER_CLASS = ".left-breakdown";
+    const OVERALL_RATING_CONTAINER_CLASS = ".breakdown-container.quality";
+    const DIFFICULTY_RATING_CONTAINER_CLASS = ".breakdown-section.difficulty";
+    const RATING_CLASS = ".grade";
+    //If the body tag has the .show_professor class, then there are ratings, otherwise, go to the next link.
+    //An instructor page can exist even though there are no ratings so this is necessary in order to not return empty values.
+    //Note: leave out the dot prefix because the JQuery hasClass() method is used.
+    const RATINGS_CLASS = "show_professor";
+    
+    if(links.length == 0){
+        if(ratings.length == 0){
+            sendResponse({results: undefined});
+        } else {
+            sendResponse({ratings:ratings});
+        }
+        return;
     }
-    return ratings;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET",links.pop(),true);
+    xhr.onload = function() {
+        parser = new DOMParser();
+        ratingPage = parser.parseFromString(xhr.responseText,"text/html");
+        if($(ratingPage.body).hasClass(RATINGS_CLASS)){
+            var overallRating = $(ratingPage).find(RATING_CONTAINER_CLASS).find(OVERALL_RATING_CONTAINER_CLASS).find(RATING_CLASS).text().trim();
+            var difficultyRating = $(ratingPage).find(RATING_CONTAINER_CLASS).find(DIFFICULTY_RATING_CONTAINER_CLASS).find(RATING_CLASS).text().trim();
+            ratings.push({overall: overallRating, difficulty: difficultyRating});
+        }
+        getRatings(links, sendResponse, ratings);
+    }
+    xhr.send();
 }
 
-//Only one function performed.
-//This is a workaround to not being able to make the http request from the content script
-//due to myfresnostate using https while ratemyprofessor uses http.
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET",request.query, false);
+    xhr.open("GET",request.query, true);
+    xhr.onload = function(){
+        var ratingLinks = extractLinks($.parseHTML(xhr.responseText, undefined));
+        if(ratingLinks == undefined) {
+            sendResponse({results: undefined});
+            return;
+        }
+        getRatings(ratingLinks,sendResponse);
+    }
     xhr.send();
-    //TODO: Evaluate performance.  Is this faster than regexp on the original string?
-    var ratingLinks = extractLinks($.parseHTML(xhr.responseText, undefined));
-    var ratings = getRatings(ratingLinks);
-    sendResponse({ratings: ratings});
+    return true;
   }
 );
